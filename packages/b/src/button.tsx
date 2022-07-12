acab3583122d37f17c37348888935b81940fcf3c
mc-petry <mc-petry@reply.io>
2022-07-13 00:17:22 +0300

    Add bug description.

--- packages/b/src/button.tsx
+++ packages/b/src/button.tsx
@@ -1,6 +1,8 @@
+// Uncomment to see a bug:
 // import * as styles from "./button.css";
 
 export function ButtonFromB() {
+  // Remove with line below to see a bug:
   return <button>Button from @pkg/b</button>;
   // return <button className={styles.button}>Button from @pkg/b</button>;
 }
